632a9ec2efcace4ef61fe26908d28330d6c2c96d
Faisal Hanif <hatilas1994@gmail.com>
2026-04-12 13:08:12 +0500

    Add new chat model implementations for Gemini and Hugging Face, update requirements.txt to include jinja2, and add .DS_Store file.

---
+++ ChatModels/chatmodel_gemini.py
@@ -0,0 +1,30 @@
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# model = ChatGoogleGenerativeAI(
+#     model="gemini-pro"
+# )
+
+# result = model.invoke("What is the capital of Pakistan?")
+
+# print(f"Answer from gemini is: {result.content}")
+
+
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# 1. Set up your API Key
+# You can also set this as an environment variable: GOOGLE_API_KEY
+os.environ["GOOGLE_API_KEY"] = "AIzaSyAt_TMlyMi1qQfF6nrIIggjnOwuUVd9OFU"
+
+# 2. Initialize the Model
+# Models include: "gemini-1.5-flash" (fast) or "gemini-1.5-pro" (complex tasks)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7)
+
+# 3. Invoke the model
+response = llm.invoke("Explain the concept of 'Schrödinger's cat' in two sentences.")
+
+# 4. Print the result
+print(response.content)
